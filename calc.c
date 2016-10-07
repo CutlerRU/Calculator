23cd0e1d04546b02f532ec6f53221d687baad018
Sam Cutler <samuel.cutler@rutgers.edu>
2016-10-07 03:37:43 -0400

    Update calc.c

--- calc.c
+++ calc.c
@@ -4,93 +4,97 @@
 
 //adds, subtracts, multiplies numbers of difference bases
 
-char * convert(char* num, char* inbase, char* outbase){
-	if(strcmp(inbase, outbase) == 0){
-		return num;
+void reverse(char** c){
+	for(int i = 0; i < (strlen(*c))/2; i++){
+		char temp = (*c)[i];
+		//printf("Temp = %c", temp);
+		//printf("Swtich = %c", (*c)[strlen(*c) - i - 1]);
+		(*c)[i] = (*c)[strlen(*c) - i - 1];
+		(*c)[strlen(*c) - i - 1] = temp;
 	}
-	char* base;
+}
 
-	if(strcmp(inbase, "b") == 0){
-		base = "10";
-	}else if(strcmp(inbase, "o") == 0){
-		base = "1000";
-	}else if(strcmp(inbase, "d") == 0){
-		base = "1010";
-	}else if(strcmp(inbase, "x") == 0){
-		base = "10000";
+char leftover(char* c){
+	if(c[strlen(c)-1] == '1')
+		return '1';
+	if(c[strlen(c)-1] == '3')
+		return '1';
+	if(c[strlen(c)-1] == '5')
+		return '1';
+	if(c[strlen(c)-1] == '7')
+		return '1';
+	if(c[strlen(c)-1] == '9')
+		return '1';
+
+	return '0';
+}
+
+char* divByTwo(char* num){
+	int digit, add, i;
+	add = 0;
+	char* x = malloc(strlen(num)+1);
+
+	for(i = 0; i < strlen(num); i++){
+		digit = num[i] - '0';
+		//printf("The number is %d\n", digit);
+		x[i] = digit/2 + add + '0';
+		if(digit % 2 == 1){
+			//printf("It made it here\n");
+			add = 5;
+		}else{
+			add = 0;
+		}
+		//printf("The value of x[%d] is: %c", i, x[i]);
+	}
+
+	if(strlen(x) != 0 && x[0] == '0'){
+		//printf("SWAGGG\n");
+		x[i] = '\0';
+		x++;
 	}else{
-		base = 0;
-		printf("ERROR!");
+		x[i] = '\0';
 	}
-	return base;
+
+	if(strlen(x) == 0){
+		free(x);
+		//printf("X was freed");
+		return 0;
+	}
+	return x;
 }
 
-char bigate(char a, char b, int * carry){
-	if(a == '0'){
-		if(b == '0'){
-			if(*carry == 0){
-				return 0;
-			}
-			*carry = 0;
-			return '1';
-		}//b == '1'
-		if(*carry == 0){
-			return '1';
-		}
-		return '0';
-	}//a == '1'
-
-	if(b == '0'){
-		if(*carry == 0){
-			return '1';
-		}
-		return '0';
-	}
-	if(*carry == 0){
-		*carry = 1;
-		return '0';
-	}
-	return '1';
-}
-
-char * add(char* num1, char* num2){
-	int max, i, j, k, len1, len2;
-	int carry = 0;
-
+char* add(char* num1, char* num2){
+	int i, j, max, a, b, carry = 0;
+	i = strlen(num1);
+	j = strlen(num2);
 	if(strlen(num1) >= strlen(num2)){
 		max = strlen(num1);
-		len1 = strlen(num1);
-		len2 = strlen(num2);
 	}else{
 		max = strlen(num2);
-		len1 = strlen(num1);
-		len2 = strlen(num2);
 	}
 
-
-	char* sum = malloc(max + 2); //the largest possible size for the sum of two numbers
+	char* sum = malloc(max+2);
 	sum[max+1] = '\0';
-
-	for(i = len1-1, j = len2-1, k = max; i >= 0 || j >= 0; i--, j--, k--){
+	for(i = strlen(num1)-1, j = strlen(num2)-1; max > 0; i--, j--, max--){
 		if(i < 0){
-			//printf("%c is digit num2", num2[i-1]);
-			sum[k] = bigate('0', num2[j], &carry);
-			//printf("%c", sum[i]);
+			b = num2[j] - '0';
+			sum[max] = (b^carry) + '0';
+			carry = b&carry;
 		}else if(j < 0){
-			//printf("%c is digit num1", num1[i-1]);
-			sum[k] = bigate('0', num1[i], &carry);
-			//printf("%c", sum[i]);
+			a = num1[i] - '0';
+			sum[max] = (a^carry) + '0';
+			carry = a&carry;
 		}else{
-			//printf("%c is digit num1 and %c is digit num2", num1[i], num2[j]);
-			sum[k] = bigate(num1[i], num2[j], &carry);
-			//printf("%c", sum[i]);
+			a = num1[i] - '0';
+			b = num2[j] - '0';
+			sum[max] = ((a^b)^carry) + '0';
+			carry = a&b || a&carry || b&carry;
 		}
 	}
-
 	if(carry == 1){
 		sum[0] = '1';
 	}else{
-		sum[0] = ' ';
+		sum++;
 	}
 
 	return sum;
@@ -98,8 +102,39 @@ char * add(char* num1, char* num2){
 
 
 int main(int argc, char** argv){
-	char*a = "111111111111111111111111111111111111111111";
-	char*b = "1";
-	printf("%s + %s = %s\n", a, b, add(a, b));
+	char* c = argv[2];
+	char* d = malloc(strlen(c)*4);
+	//printf("c = %s\n", c);
+	char* a = argv[1];
+	char* b = malloc(strlen(a)*4);
+	int i = 0;
+	//printf("HELLO");
+	//printf("\n%s", divByTwo(a));
+	for(i = 0; a != 0; i++){
+		//printf("b[%d]= %c \n", i, leftover(a));
+		b[i] = leftover(a);
+		//printf("\n%s", a);
+		a = divByTwo(a);
+		//printf("Step %d: %s\n", i, a);
+		//printf("Strlen[%d] = %c" , strlen(a) ,a[strlen(a) -1]);
+	}
+	b[i] = '\0';
+	//printf("The value is = %s", b);
+	reverse(&b);
+	//printf("HELLO");
+	for(i = 0; c != 0; i++){
+		//printf("b[%d]= %c \n", i, leftover(a));
+		d[i] = leftover(c);
+		//printf("\n%s", a);
+		c = divByTwo(c);
+		//printf("Step %d: %s\n", i, a);
+		//printf("Strlen[%d] = %c" , strlen(a) ,a[strlen(a) -1]);
+	}
+	d[i] = '\0';
+	//printf("The value is = %s", d);
+	reverse(&d);
+
+	printf("\n%s + %s = %s\n", b, d, add(b,d));
+
 	return 0;
 }
